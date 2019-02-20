44676b3a1983a4601e248e8a83c0e1f18f56f5ce
MatheusMorett2 <matheusmorett300@gmail.com>
2019-02-19 21:20:57 -0300

    build

---
+++ build/precache-manifest.482e447d4e825b47e6d4a6fe60f8031c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8a7e78dffb37b41fd780",
+    "url": "/static/js/main.8a7e78df.chunk.js"
+  },
+  {
+    "revision": "7944d3b74af946ac9fe5",
+    "url": "/static/js/2.7944d3b7.chunk.js"
+  },
+  {
+    "revision": "8a7e78dffb37b41fd780",
+    "url": "/static/css/main.68b4bb81.chunk.css"
+  },
+  {
+    "revision": "518b28fba5fa38f90cd1f65e7b778dc4",
+    "url": "/index.html"
+  }
+];
